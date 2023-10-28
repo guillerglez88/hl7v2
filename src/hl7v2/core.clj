6fbcd1303ba26757c977954dc8fcee5d99523c63
guillerglez88 <guillerglez88@gmail.com>
2023-10-29 00:25:52 +0200

    (char-seq rdr) fn for streaming chars

--- src/hl7v2/core.clj
+++ src/hl7v2/core.clj
@@ -1,1 +1,15 @@
-(ns hl7v2.core)
+(ns hl7v2.core
+  (:require
+   [clojure.java.io :as io])
+  (:import
+   (java.io Reader)))
+
+;; TODO: use (with-open ...)
+(def ^Reader rdr (io/reader "test/data/sample.hl7"))
+
+(defn char-seq [^Reader rdr]
+  (let [ch (.read rdr)]
+    (when-not (= ch -1)
+      (cons (char ch) (lazy-seq (char-seq rdr))))))
+
+(apply str (char-seq rdr))
