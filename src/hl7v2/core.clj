3ff4f7b0c1f1acd1afcfed6156f4f4a2a7b51229
Guillermo Rodríguez González <guille@guillermos-macbook-pro.home>
2024-01-02 12:59:07 +0100

    refactoring format to support new data model and fixing bug in parser

--- src/hl7v2/core.clj
+++ src/hl7v2/core.clj
@@ -1,52 +1,145 @@
 (ns hl7v2.core
   (:require
-   [clojure.string :as str])
-  (:refer-clojure :exclude [format]))
+   [clojure.string :as str]
+   [clojure.java.io :as io])
+  (:refer-clojure :exclude [format])
+  (:import
+   (java.io Reader)))
 
-(defn head->tag [head]
-  (let [tags (reverse [:fld :rep :cmp :sub])]
-    (->> (reverse head)
-         (map vector tags)
-         (filter #(not= 0 (second %)))
-         (map first)
-         (first))))
+(defn- char-seq [^Reader rdr]
+  (let [ch (.read rdr)]
+    (when-not (= ch -1)
+      (cons (char ch) (lazy-seq (char-seq rdr))))))
 
-(defn fill-segment [seg]
-  (let [[id data] seg
-        pad-head (fn [head]
-                   (first (partition 4 4 [0 0 0 0] head)))]
-    (->> (map (juxt (comp vec pad-head first) second) data)
-         (cons [[1 0 0 0] ""])
-         (sort-by first)
-         (partition 2 1)
-         (mapcat (fn [[prev next]]
-                   (concat [prev]
-                           (map (fn [fld] [[fld 0 0 0] ""])
-                                (range (inc (get-in prev [0 0]))
-                                       (get-in next [0 0])))
-                           [next])))
-         (distinct)
-         (into {})
-         (vector id))))
+(defn- tokenize [^Reader rdr]
+  (let [[m s h fld cmp rep esc sub & rest] (char-seq rdr)
+        seg (str m s h)
+        encoding (str cmp rep esc sub)
+        mark-escaped (fn [[fst snd]]
+                       (cond
+                         (= esc fst) {:val snd, :escaped true}
+                         (= esc snd) nil
+                         :else       {:val snd}))
+        separator? (fn [item]
+                     (when (and (#{fld cmp rep sub \return \newline} (:val item))
+                                (not (:escaped item)))
+                       (gensym)))]
+    (->> (concat [nil] rest [nil])
+         (partition-all 2 1)
+         (map mark-escaped)
+         (remove nil?)
+         (partition-by separator?)
+         (map #(apply str (map :val %)))
+         (cons encoding)
+         (cons (str fld))
+         (cons seg))))
 
-(defn format-segment [seg opts]
-  (let [[id data] seg
-        msh-1? (fn [head]
-                 (and (= id "MSH")
-                      (= head [1 0 0 0])))]
-    (->> (sort-by first data)
-         (remove (comp msh-1? first))
-         (mapcat (juxt (comp head->tag first) second))
-         (map #(opts % %))
-         (apply str)
-         (str id))))
+(defn- chunks-by [pred coll]
+  (->> coll
+       (partition-by #(when (pred %)
+                        (gensym)))
+       (partition-all 2 1)
+       (map (fn [[fst snd]]
+              (when (pred (first fst))
+                (if-not (pred (first snd))
+                  snd
+                  (first fst)))))
+       (remove nil?)
+       (map #(if (pred %) nil %))))
+
+(defn- nest [separators offset coll]
+  (if (seq separators)
+    (let [{t :tag, s :val} (first separators)
+          chunks (->> (cons s coll)
+                      (chunks-by #{s})
+                      (map #(when (seq? %)
+                              (nest (next separators) 1 %))))]
+      (cond
+        (= :fld t) (->> chunks
+                        (zipmap (drop offset (range)))
+                        (remove (comp nil? second))
+                        (into {}))
+        (#{:cmp :sub} t) (let [entries (->> chunks
+                                            (zipmap (drop offset (range)))
+                                            (remove (comp nil? second)))]
+                           (case (count entries)
+                             0 nil
+                             1 (second (first entries))
+                             (into {} entries)))
+        (= :rep t) (let [entries (->> chunks
+                                      (reverse)
+                                      (drop-while nil?)
+                                      (reverse))]
+                     (if (= 1 (count entries))
+                       (first entries)
+                       (into [] entries)))
+        :else chunks))
+    (apply str coll)))
+
+(defn parse [x]
+  (with-open [rdr (io/reader x)]
+    (let [tokens (tokenize rdr)
+          [seg fld [cmp rep esc sub]] tokens
+          separators (map (fn [t s] {:tag t, :val (str s)})
+                          [:fld :rep :cmp :sub]
+                          [fld rep cmp sub])]
+      (when (not= "MSH" seg)
+        (throw (ex-info "Message should start with MSH" {:seg seg})))
+      (->> tokens
+           (partition-by #{"\r" "\n"})
+           (remove #{'("\r") '("\n")})
+           (map (fn [[seg fld & data]]
+                  {(keyword seg) (if (= "MSH" seg)
+                                   (assoc (nest separators 2 data)
+                                          1 fld)
+                                   (nest separators 1 data))}))
+           (into [])))))
 
 (defn format [hl7 & {:keys [line-break] :or {line-break "\r\n"}}]
-  (let [segments (mapv fill-segment hl7)
-        msh (some #(when (= (first %) "MSH") (second %)) segments)
-        fld (msh [1 0 0 0])
-        [cmp rep _esc sub] (seq (msh [2 0 0 0]))
-        opts (zipmap [:fld :rep :cmp :sub] [fld rep cmp sub])]
-    (->> segments
-         (map #(format-segment % opts))
-         (str/join line-break))))
+  (letfn [(encode [data separators]
+            (cond
+              (vector? data) (->> data
+                                  (map #(encode % (next separators)))
+                                  (str/join (first separators)))
+              (map? data) (let [last (->> (keys data) (apply max))]
+                            (->> (range 1 (inc last))
+                                 (map #(encode (get data %) (next separators)))
+                                 (str/join (first separators))))
+              :else data))]
+    (let [msh (some :MSH hl7)
+          fld (get msh 1)
+          [cmp rep _esc sub] (seq (get msh 2))]
+      (str/join line-break
+                (for [seg hl7
+                      :let [[id data] (first seg)]]
+                  (if (= :MSH id)
+                    (str (name id)
+                         (encode (dissoc data 1) [fld rep cmp sub]))
+                    (str (name id)
+                         fld
+                         (encode data [fld rep cmp sub]))))))))
+
+(comment
+  (parse (.getBytes (str "MSH|^~\\&\r\n"
+                         "PID|||123456||Doe^John")))
+
+  (format [{:MSH {1 "|", 2 "^~\\&"}}
+           {:PID {3 [{1 "000197245",
+                      4 {1 "NationalPN", 2 "2.16.840.1.113883.19.3", 3 "ISO"},
+                      5 "PN"}
+                     {1 "4532",
+                      4 {1 "Careful&CareClinic", 2 "2.16.840.1.113883.19.2.400566", 3 "ISO"},
+                      5 "PI"}
+                     {1 "3242346",
+                      4 {1 "GoodmanGP", 2 "2.16.840.1.113883.19.2.450998", 3 "ISO"},
+                      5 "PI"}],
+                  5 {1 "Patient", 2 "Particia", 7 "L"},
+                  7 "19750103",
+                  8 "F"
+                  11 {1 {1 "Randomroad 23a", 2 "Randomroad", 3 "23a"},
+                      3 "Anytown",
+                      5 "1200",
+                      7 "H"},
+                  13 [{1 "555 3542557", 2 "ORN", 3 "PH"}
+                      {1 "555 3542558", 2 "ORN", 3 "FX"}],
+                  14 {1 "555 5557865", 2 "WPN", 3 "PH"}}}]))
--- test/hl7v2/core_test.clj
+++ test/hl7v2/core_test.clj
@@ -5,104 +5,63 @@
 
 (t/deftest parse-test
   (t/testing "Parse hl7 string"
-    (t/is (= [["MSH"
-               {[9 0 1] "R01",
-                [4] "TML",
-                [7] "200905011130",
-                [10] "20169838-v25",
-                [6] "OLIS",
-                [12] "2.5",
-                [9] "ORU",
-                [3] "ULTRA",
-                [5] "OLIS",
-                [11] "T",
-                [2] "^~\\&",
-                [1] \|}]
-              ["PID"
-               {[3 0 4] "MR",
-                [7] "19310313",
-                [11 0 4] "M6G 2T9",
-                [11 0 2] "TORONTO",
-                [13] "(416)888-8888",
-                [3] "7005728",
-                [5 0 2] "DIAMOND",
-                [8] "F",
-                [5] "TEST",
-                [5 0 1] "RACHEL",
-                [3 0 3] "TML",
-                [19] "1014071185",
-                [11] "200 ANYWHERE ST",
-                [11 0 3] "ON",
-                [19 0 1] "KR"}]
-              ["PV1"
-               {[1] "1",
-                [3] "OLIS",
-                [7] "OLIST",
-                [7 0 1] "BLAKE",
-                [7 0 2] "DONALD",
-                [7 0 3] "THOR",
-                [7 0 8] "921379",
-                [7 0 12] "OLIST"}]
-              ["ORC"
-               {[12 0 2] "DONALD",
-                [12] "OLIST",
-                [12 0 7] "L",
-                [3 0 2] "OLIS_Site_ID",
-                [3] "T09-100442-RET-0",
-                [12 0 8] "921379",
-                [3 0 3] "ISO",
-                [1] "RE",
-                [12 0 3] "THOR",
-                [12 0 1] "BLAKE"}]
-              ["OBR"
-               {[4] "RET",
-                [7] "200905011106",
-                [16 0 7] "L",
-                [20] "T09-100442",
-                [22] "200905011130",
-                [3 0 2] "OLIS_Site_ID",
-                [3] "T09-100442-RET-0",
-                [4 0 1] "RETICULOCYTE COUNT",
-                [24] "B7",
-                [16 0 8] "921379",
-                [16] "OLIST",
-                [16 0 3] "THOR",
-                [16 0 1] "BLAKE",
-                [14] "200905011106",
-                [27] "1",
-                [3 0 3] "ISO",
-                [21] "MOHLTC",
-                [19] "7870279",
-                [16 0 2] "DONALD",
-                [27 0 3] "200905011106",
-                [4 0 2] "HL79901 literal",
-                [1] "0",
-                [27 0 5] "R",
-                [25] "F",
-                [18] "7870279"}]
-              ["OBX"
-               {[1] "1",
-                [2] "ST",
-                [5] "Test Value"}]]
+    (t/is (= [{:MSH {1 "|",
+                     2 "^~\\&",
+                     3 "ULTRA",
+                     4 "TML",
+                     5 "OLIS",
+                     6 "OLIS",
+                     7 "200905011130",
+                     9 {1 "ORU", 2 "R01"},
+                     10 "20169838-v25",
+                     11 "T",
+                     12 "2.5"}}
+              {:PID {7 "19310313",
+                     13 "(416)888-8888",
+                     3 {1 "7005728", 4 "TML", 5 "MR"},
+                     19 {1 "1014071185", 2 "KR"},
+                     11 {1 "200 ANYWHERE ST", 3 "TORONTO", 4 "ON", 5 "M6G 2T9"},
+                     5 {1 "TEST", 2 "RACHEL", 3 "DIAMOND"},
+                     8 "F"}}
+              {:PV1 {1 "1",
+                     3 "OLIS",
+                     7 {1 "OLIST",
+                        2 "BLAKE",
+                        3 "DONALD",
+                        4 "THOR",
+                        9 "921379",
+                        13 "OLIST"}}}
+              {:ORC {1 "RE",
+                     3 {1 "T09-100442-RET-0", 3 "OLIS_Site_ID", 4 "ISO"},
+                     12 {1 "OLIST", 4 "THOR", 3 "DONALD", 2 "BLAKE", 9 "921379", 8 "L"}}}
+              {:OBR {1 "0",
+                     3 {1 "T09-100442-RET-0", 3 "OLIS_Site_ID", 4 "ISO"},
+                     4 {1 "RET", 2 "RETICULOCYTE COUNT", 3 "HL79901 literal"},
+                     7 "200905011106",
+                     14 "200905011106",
+                     16 {1 "OLIST", 4 "THOR", 3 "DONALD", 2 "BLAKE", 9 "921379", 8 "L"},
+                     18 "7870279",
+                     19 "7870279",
+                     20 "T09-100442",
+                     21 "MOHLTC",
+                     22 "200905011130",
+                     24 "B7",
+                     25 "F",
+                     27 {1 "1", 4 "200905011106", 6 "R"}}}
+              {:OBX {1 "1", 2 "ST", 5 "Test Value"}}]
              (sut/parse (.getBytes (str "MSH|^~\\&|ULTRA|TML|OLIS|OLIS|200905011130||ORU^R01|20169838-v25|T|2.5\r\n"
                                         "PID|||7005728^^^TML^MR||TEST^RACHEL^DIAMOND||19310313|F|||200 ANYWHERE ST^^TORONTO^ON^M6G 2T9||(416)888-8888||||||1014071185^KR\r\n"
                                         "PV1|1||OLIS||||OLIST^BLAKE^DONALD^THOR^^^^^921379^^^^OLIST\r\n"
                                         "ORC|RE||T09-100442-RET-0^^OLIS_Site_ID^ISO|||||||||OLIST^BLAKE^DONALD^THOR^^^^L^921379\r\n"
                                         "OBR|0||T09-100442-RET-0^^OLIS_Site_ID^ISO|RET^RETICULOCYTE COUNT^HL79901 literal|||200905011106|||||||200905011106||OLIST^BLAKE^DONALD^THOR^^^^L^921379||7870279|7870279|T09-100442|MOHLTC|200905011130||B7|F||1^^^200905011106^^R\r\n"
                                         "OBX|1|ST|||Test Value")))))
-    (t/is (= [["MSH"
-               {[1] \|,
-                [2] "^~\\&",
-                [3] "TestSendingSystem",
-                [7] "200701011539",
-                [9] "ADT",
-                [9 0 1] "A01",
-                [9 0 2] "ADT A01",
-                [13] "123"}]
-              ["PID"
-               {[3] "123456",
-                [5] "Doe",
-                [5 0 1] "John"}]]
+    (t/is (= [{:MSH {1 "|",
+                     2 "^~\\&",
+                     3 "TestSendingSystem",
+                     7 "200701011539",
+                     9 {1 "ADT", 2 "A01", 3 "ADT A01"},
+                     13 "123"}}
+              {:PID {3 "123456", 5 {1 "Doe", 2 "John"}}}]
              (sut/parse (.getBytes (str "MSH|^~\\&|TestSendingSystem||||200701011539||ADT^A01^ADT A01||||123\r\n"
                                         "PID|||123456||Doe^John")))))))
 
@@ -110,19 +69,13 @@
   (t/testing "Format hl7 message"
     (t/is (= (str "MSH|^~\\&|TestSendingSystem||||200701011539||ADT^A01^ADT A01||||123\r\n"
                   "PID|||123456||Doe^John")
-             (sut/format [["MSH"
-                           {[1] "|"
-                            [2] "^~\\&"
-                            [3] "TestSendingSystem"
-                            [7] "200701011539"
-                            [9] "ADT"
-                            [9 0 1] "A01"
-                            [9 0 2] "ADT A01"
-                            [13] "123"}]
-                          ["PID"
-                           {[3] "123456"
-                            [5] "Doe"
-                            [5 0 1] "John"}]]))))
+             (sut/format [{:MSH {1 "|",
+                                 2 "^~\\&",
+                                 3 "TestSendingSystem",
+                                 7 "200701011539",
+                                 9 {1 "ADT", 2 "A01", 3 "ADT A01"},
+                                 13 "123"}}
+                          {:PID {3 "123456", 5 {1 "Doe", 2 "John"}}}]))))
   (t/testing "Format segment"
     (t/is (= (str "MSH|^~\\&\r\n"
                   "PID|||123456||Doe^John")
