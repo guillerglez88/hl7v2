b245886a5028e56a0fed3b2a5eb2ce0c1f1d0d12
Guillermo <guillerglez88@gmail.com>
2024-04-14 01:11:22 +0200

    adding structure fn

--- README.md
+++ README.md
@@ -67,6 +67,104 @@ Encode data into hl7 string.
   ;;     PID|||123456||Doe^John"
 ```
 
+## structure
+
+Structure parsed hl7 according to a message structure
+
+```clojure
+  (let [str-msg (str "MSH|^~\\&|ULTRA|TML|OLIS|OLIS|200905011130||ORU^R01|20169838-v25|T|2.5\r\n"
+                     "PID|||7005728^^^TML^MR||TEST^RACHEL^DIAMOND||19310313|F|||200 ANYWHERE ST^^TORONTO^ON^M6G 2T9||(416)888-8888||||||1014071185^KR\r\n"
+                     "PV1|1||OLIS||||OLIST^BLAKE^DONALD^THOR^^^^^921379^^^^OLIST\r\n"
+                     "ORC|RE||T09-100442-RET-0^^OLIS_Site_ID^ISO|||||||||OLIST^BLAKE^DONALD^THOR^^^^L^921379\r\n"
+                     "OBR|0||T09-100442-RET-0^^OLIS_Site_ID^ISO|RET^RETICULOCYTE COUNT^HL79901 literal|||200905011106|||||||200905011106||OLIST^BLAKE^DONALD^THOR^^^^L^921379||7870279|7870279|T09-100442|MOHLTC|200905011130||B7|F||1^^^200905011106^^R\r\n"
+                     "OBX|1|ST|||Test Value")
+        hl7 (parse (.getBytes str-msg))]
+    (structure [:ORU_R01 {:version "2.5.1"}
+                [:MSH]
+                [:SFT]
+                [:PATIENT-RESULTS
+                 [:PATIENT
+                  [:PID]
+                  [:PD1]
+                  [:NTE]
+                  [:NK1]
+                  [:VISIT
+                   [:PV1]
+                   [:PV2]]]
+                 [:ORDER-OBSERVATION
+                  [:ORC]
+                  [:OBR]
+                  [:NTE]
+                  [:TIMING-QTY
+                   [:TQ1]
+                   [:TQ2]]
+                  [:CTD]
+                  [:OBSERVATION
+                   [:OBX]
+                   [:NTE]]
+                  [:FT1]
+                  [:CTI]
+                  [:SPECIMEN
+                   [:SPM]
+                   [:OBX]]]]
+                [:DSC]] hl7))
+  ;; => {:ORU_R01
+  ;;     ({:MSH
+  ;;       {1 "|",
+  ;;        2 "^~\\&",
+  ;;        3 "ULTRA",
+  ;;        4 "TML",
+  ;;        5 "OLIS",
+  ;;        6 "OLIS",
+  ;;        7 "200905011130",
+  ;;        9 {1 "ORU", 2 "R01"},
+  ;;        10 "20169838-v25",
+  ;;        11 "T",
+  ;;        12 "2.5"}}
+  ;;      {:PATIENT-RESULTS
+  ;;       ({:PATIENT
+  ;;         ({:PID
+  ;;           {3 {1 "7005728", 4 "TML", 5 "MR"},
+  ;;            5 {1 "TEST", 2 "RACHEL", 3 "DIAMOND"},
+  ;;            7 "19310313",
+  ;;            8 "F",
+  ;;            11 {1 "200 ANYWHERE ST", 3 "TORONTO", 4 "ON", 5 "M6G 2T9"},
+  ;;            13 "(416)888-8888",
+  ;;            19 {1 "1014071185", 2 "KR"}}}
+  ;;          {:VISIT
+  ;;           ({:PV1
+  ;;             {1 "1",
+  ;;              3 "OLIS",
+  ;;              7
+  ;;              {1 "OLIST",
+  ;;               2 "BLAKE",
+  ;;               3 "DONALD",
+  ;;               4 "THOR",
+  ;;               9 "921379",
+  ;;               13 "OLIST"}}})})}
+  ;;        {:ORDER-OBSERVATION
+  ;;         ({:ORC
+  ;;           {1 "RE",
+  ;;            3 {1 "T09-100442-RET-0", 3 "OLIS_Site_ID", 4 "ISO"},
+  ;;            12 {1 "OLIST", 2 "BLAKE", 3 "DONALD", 4 "THOR", 8 "L", 9 "921379"}}}
+  ;;          {:OBR
+  ;;           {1 "0",
+  ;;            3 {1 "T09-100442-RET-0", 3 "OLIS_Site_ID", 4 "ISO"},
+  ;;            4 {1 "RET", 2 "RETICULOCYTE COUNT", 3 "HL79901 literal"},
+  ;;            7 "200905011106",
+  ;;            14 "200905011106",
+  ;;            16 {1 "OLIST", 2 "BLAKE", 3 "DONALD", 4 "THOR", 8 "L", 9 "921379"},
+  ;;            18 "7870279",
+  ;;            19 "7870279",
+  ;;            20 "T09-100442",
+  ;;            21 "MOHLTC",
+  ;;            22 "200905011130",
+  ;;            24 "B7",
+  ;;            25 "F",
+  ;;            27 {1 "1", 4 "200905011106", 6 "R"}}}
+  ;;          {:OBSERVATION ({:OBX {1 "1", 2 "ST", 5 "Test Value"}})})})})}
+```
+
 ---
 
 Standard: https://www.hl7.org/implement/standards/product_brief.cfm?product_id=185   
--- src/hl7v2/core.clj
+++ src/hl7v2/core.clj
@@ -189,3 +189,128 @@
                     (str (name id)
                          (:field encoding)
                          (encode data separators))))))))
+
+(defn structure [trigger-event hl7]
+  (letfn [(extract [[h & t]]
+            (if (map? (first t))
+              (let [[attrs & children] t]
+                {:tag h
+                 :attrs attrs
+                 :group? (seq children)
+                 :children children})
+              {:tag h
+               :group? (seq t)
+               :children t}))
+          (match [[item & items] [seg & segments]]
+            (if (and item seg)
+              (let [{:keys [tag attrs group? children]} (extract item)]
+                (if group?
+                  (let [[data segs] (reduce (fn [acc curr]
+                                              (let [[data segs] (match [curr] (second acc))]
+                                                [(concat (first acc) data) segs]))
+                                            [nil (cons seg segments)]
+                                            children)]
+                    (if (seq data)
+                      [[{tag data}] segs]
+                      [nil segs]))
+                  (if (get seg tag)
+                    [[seg] segments]
+                    (match items (cons seg segments)))))
+              (when-not item
+                [nil (cons seg segments)])))]
+    (ffirst (match [trigger-event] hl7))))
+
+(comment
+  (let [str-msg (str "MSH|^~\\&|ULTRA|TML|OLIS|OLIS|200905011130||ORU^R01|20169838-v25|T|2.5\r\n"
+                     "PID|||7005728^^^TML^MR||TEST^RACHEL^DIAMOND||19310313|F|||200 ANYWHERE ST^^TORONTO^ON^M6G 2T9||(416)888-8888||||||1014071185^KR\r\n"
+                     "PV1|1||OLIS||||OLIST^BLAKE^DONALD^THOR^^^^^921379^^^^OLIST\r\n"
+                     "ORC|RE||T09-100442-RET-0^^OLIS_Site_ID^ISO|||||||||OLIST^BLAKE^DONALD^THOR^^^^L^921379\r\n"
+                     "OBR|0||T09-100442-RET-0^^OLIS_Site_ID^ISO|RET^RETICULOCYTE COUNT^HL79901 literal|||200905011106|||||||200905011106||OLIST^BLAKE^DONALD^THOR^^^^L^921379||7870279|7870279|T09-100442|MOHLTC|200905011130||B7|F||1^^^200905011106^^R\r\n"
+                     "OBX|1|ST|||Test Value")
+        hl7 (parse (.getBytes str-msg))]
+    (structure [:ORU_R01 {:version "2.5.1"}
+                [:MSH]
+                [:SFT]
+                [:PATIENT-RESULTS
+                 [:PATIENT
+                  [:PID]
+                  [:PD1]
+                  [:NTE]
+                  [:NK1]
+                  [:VISIT
+                   [:PV1]
+                   [:PV2]]]
+                 [:ORDER-OBSERVATION
+                  [:ORC]
+                  [:OBR]
+                  [:NTE]
+                  [:TIMING-QTY
+                   [:TQ1]
+                   [:TQ2]]
+                  [:CTD]
+                  [:OBSERVATION
+                   [:OBX]
+                   [:NTE]]
+                  [:FT1]
+                  [:CTI]
+                  [:SPECIMEN
+                   [:SPM]
+                   [:OBX]]]]
+                [:DSC]] hl7))
+  ;; => {:ORU_R01
+  ;;     ({:MSH
+  ;;       {1 "|",
+  ;;        2 "^~\\&",
+  ;;        3 "ULTRA",
+  ;;        4 "TML",
+  ;;        5 "OLIS",
+  ;;        6 "OLIS",
+  ;;        7 "200905011130",
+  ;;        9 {1 "ORU", 2 "R01"},
+  ;;        10 "20169838-v25",
+  ;;        11 "T",
+  ;;        12 "2.5"}}
+  ;;      {:PATIENT-RESULTS
+  ;;       ({:PATIENT
+  ;;         ({:PID
+  ;;           {3 {1 "7005728", 4 "TML", 5 "MR"},
+  ;;            5 {1 "TEST", 2 "RACHEL", 3 "DIAMOND"},
+  ;;            7 "19310313",
+  ;;            8 "F",
+  ;;            11 {1 "200 ANYWHERE ST", 3 "TORONTO", 4 "ON", 5 "M6G 2T9"},
+  ;;            13 "(416)888-8888",
+  ;;            19 {1 "1014071185", 2 "KR"}}}
+  ;;          {:VISIT
+  ;;           ({:PV1
+  ;;             {1 "1",
+  ;;              3 "OLIS",
+  ;;              7
+  ;;              {1 "OLIST",
+  ;;               2 "BLAKE",
+  ;;               3 "DONALD",
+  ;;               4 "THOR",
+  ;;               9 "921379",
+  ;;               13 "OLIST"}}})})}
+  ;;        {:ORDER-OBSERVATION
+  ;;         ({:ORC
+  ;;           {1 "RE",
+  ;;            3 {1 "T09-100442-RET-0", 3 "OLIS_Site_ID", 4 "ISO"},
+  ;;            12 {1 "OLIST", 2 "BLAKE", 3 "DONALD", 4 "THOR", 8 "L", 9 "921379"}}}
+  ;;          {:OBR
+  ;;           {1 "0",
+  ;;            3 {1 "T09-100442-RET-0", 3 "OLIS_Site_ID", 4 "ISO"},
+  ;;            4 {1 "RET", 2 "RETICULOCYTE COUNT", 3 "HL79901 literal"},
+  ;;            7 "200905011106",
+  ;;            14 "200905011106",
+  ;;            16 {1 "OLIST", 2 "BLAKE", 3 "DONALD", 4 "THOR", 8 "L", 9 "921379"},
+  ;;            18 "7870279",
+  ;;            19 "7870279",
+  ;;            20 "T09-100442",
+  ;;            21 "MOHLTC",
+  ;;            22 "200905011130",
+  ;;            24 "B7",
+  ;;            25 "F",
+  ;;            27 {1 "1", 4 "200905011106", 6 "R"}}}
+  ;;          {:OBSERVATION ({:OBX {1 "1", 2 "ST", 5 "Test Value"}})})})})}
+
+  :.)
