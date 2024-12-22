906ba1cf762526c9949543eb2fa685526ac9b53b
Guillermo <guillerglez88@gmail.com>
2024-12-22 23:51:10 +0100

    trigger event structure hiccup

---
+++ src/hl7v2/config.clj
@@ -0,0 +1,5 @@
+(ns hl7v2.config)
+
+(def config
+  (delay
+    {:standard-dir "tmp"}))
--- src/hl7v2/schema.clj
+++ src/hl7v2/schema.clj
@@ -4,7 +4,8 @@
    [clojure.java.io :as io]
    [clojure.string :as str]
    [clojure.walk :refer [postwalk]]
-   [hl7v2.complex :refer [clean]])
+   [hl7v2.complex :refer [clean]]
+   [hl7v2.config :refer [config]])
   (:import
    (java.text Normalizer Normalizer$Form)))
 
@@ -49,6 +50,27 @@
          (drop (if (node-attrs node) 2 1))
          (into []))))
 
+(defn node-annotation [node]
+  (letfn [(rm-diacritics [s]
+            (when s
+              (-> s
+                  (Normalizer/normalize Normalizer$Form/NFD)
+                  (str/replace #"[\u0300-\u036f]" ""))))]
+    (->> (node-children node)
+         (some #(when (and (= "documentation" (first %))
+                           (= "en" (:lang (second %))))
+                  (->> (str/split (last %) #"\s+")
+                       (map (fn [s]
+                              (-> (rm-diacritics s)
+                                  (str/replace #"/" "Or")
+                                  (str/replace #"-" ""))))
+                       (str/join "")))))))
+
+(defn type-base [node]
+  (->> (node-children node)
+       (filter (comp #{"extension"} node-tag))
+       (some (comp :base node-attrs))))
+
 (defn content-node? [node]
   (when-let [attrs (node-attrs node)]
     (= (str (node-tag node) ".CONTENT")
@@ -64,93 +86,81 @@
          (when-let [attrs (node-attrs node)]
            (some? (:minOccurs attrs))))))
 
-(letfn [(load-schema []
-          (let [files ["tmp/HL7-xml v2.5.1/ORU_R01.xsd"
-                       "tmp/HL7-xml v2.5.1/segments.xsd"
-                       "tmp/HL7-xml v2.5.1/datatypes.xsd"
-                       "tmp/HL7-xml v2.5.1/fields.xsd"]]
-            (->> (for [f files]
-                   (-> (io/file f)
-                       (parse-xsd)
-                       (index-schema)))
-                 (apply merge))))
-        (fill-node [node index]
-          (let [tag (node-tag node)
-                attrs (node-attrs node)
-                children (node-children node)]
-            (cond
-              (content-node? node) (let [content (get index (:type attrs))
-                                         node (fill-node content index)]
-                                     (->> (node-children node)
-                                          (mapcat (fn [n]
-                                                    (if (sequence-node? n)
-                                                      (fill-node n index)
-                                                      [(fill-node n index)])))
-                                          (concat [tag (merge attrs (dissoc (node-attrs node) :type))])
-                                          (into [])))
-              (sequence-node? node) (->> (for [node children]
-                                           (fill-node node index))
-                                         (into []))
-              (group-node? node) (let [group (get index tag)
-                                       node (fill-node group index)
-                                       tag (last (str/split tag #"\."))]
-                                   (->> (node-children node)
-                                        (concat [tag (merge attrs (dissoc (node-attrs node) :type))])
-                                        (into [])))
+(defn segment-node? [node]
+  (when-let [tag (node-tag node)]
+    (and (= 1 (count (str/split tag #"\.")))
+         (when-let [attrs (node-attrs node)]
+           (some? (:minOccurs attrs))))))
 
-              :else node)))]
-  (let [tgr-evt "ORU_R01"
-        index (load-schema)
-        root (get index tgr-evt)]
-    (fill-node root index)
-    #_(get index "ORU_R01.CONTENT")))
+(defn annotation-node? [node]
+  (when-let [tag (node-tag node)]
+    (= "annotation" tag)))
 
-(defn sequence-item [sequence]
-  [(first (str/split (first sequence) #"\."))
-   (->> sequence
-        (filter vector?)
-        (filter (comp #{"sequence"} first))
-        (mapcat (partial drop 1))
-        (remove (comp #{"any"} first))
-        (map (juxt #(-> % first (str/split #"\.") last parse-long) second))
-        (sort-by (fnil first 0))
-        (into (sorted-map)))])
+(defn type-node? [node]
+  (when-let [tag (node-tag node)]
+    (#{"complexContent" "simpleContent"} tag)))
 
-(defn content-item [content]
-  (letfn [(rm-diacritics [s]
-            (when s
-              (-> s
-                  (Normalizer/normalize Normalizer$Form/NFD)
-                  (str/replace #"[\u0300-\u036f]" ""))))]
-    [(->> content
-          (filter vector?)
-          (filter (comp #{"annotation"} first))
-          (mapcat (partial drop 1))
-          (some #(when (and (= "documentation" (first %))
-                            (= "en" (:lang (second %))))
-                   (->> (str/split (last %) #"\s+")
-                        (map rm-diacritics)
-                        (str/join "")))))
-     {:type (->> content
-                 (filter vector?)
-                 (filter (comp #{"complexContent"} first))
-                 (mapcat (partial drop 1))
-                 (some #(when (= "extension" (first %))
-                          (:base (second %)))))}]))
+(defn fill-node [node index]
+  (let [tag (node-tag node)
+        attrs (node-attrs node)
+        children (node-children node)]
+    (cond
+      (= "any" tag) nil
+      (content-node? node) (let [content (get index (:type attrs))
+                                 node (fill-node content index)]
+                             (->> (node-children node)
+                                  (mapcat (fn [n]
+                                            (if (sequence-node? n)
+                                              (fill-node n index)
+                                              [(fill-node n index)])))
+                                  (concat [tag (merge attrs (node-attrs node))])
+                                  (into [])))
+      (sequence-node? node) (->> (for [node children]
+                                   (fill-node node index))
+                                 (into []))
+      (group-node? node) (let [group (get index tag)
+                               node (fill-node group index)
+                               tag (last (str/split tag #"\."))
+                               children (node-children node)]
+                           (->> children
+                                (drop-while string?)
+                                (concat [tag (merge attrs (node-attrs node) {:name (first (filter string? children))})])
+                                (into [])))
+      (segment-node? node) (let [node (get index tag)]
+                             (fill-node (->> (node-children node)
+                                             (concat [tag (merge attrs (node-attrs node))])
+                                             (into []))
+                                        index))
+      (annotation-node? node) (node-annotation node)
+      (type-node? node) (let [node (get index (type-base node))]
+                          (->> (node-children node)
+                               (mapcat (fn [n]
+                                         (if (sequence-node? n)
+                                           (fill-node n index)
+                                           [(fill-node n index)])))
+                               (concat [(node-tag node)])
+                               (into [])))
+      :else node)))
+
+(defn gen-structure [trigger-event]
+  (letfn [(load-schema [tgr-evt]
+            (let [dir (:standard-dir @config)
+                  files [(format "%s/%s.xsd" dir tgr-evt)
+                         (format "%s/segments.xsd" dir)
+                         (format "%s/datatypes.xsd" dir)
+                         (format "%s/fields.xsd" dir)]]
+              (->> (for [f files]
+                     (-> (io/file f)
+                         (parse-xsd)
+                         (index-schema)))
+                   (apply merge))))]
+    (let [index (load-schema trigger-event)
+          root (get index trigger-event)]
+      (fill-node root index))))
 
 (comment
 
-  (parse-xsd (io/file "test/hl7v2/data/ACK.xsd"))
-  ;;=> ["schema"
-  ;;    {:targetNamespace "urn:hl7-org:v2xml", :version "1.1"}
-  ;;    ["include" {:schemaLocation "segments.xsd"}]
-  ;;    ["ACK.CONTENT"
-  ;;     ["sequence"
-  ;;      ["MSH" {:minOccurs "1", :maxOccurs "1"}]
-  ;;      ["SFT" {:minOccurs "0", :maxOccurs "unbounded"}]
-  ;;      ["MSA" {:minOccurs "1", :maxOccurs "1"}]
-  ;;      ["ERR" {:minOccurs "0", :maxOccurs "unbounded"}]]]
-  ;;    ["ACK" {:type "ACK.CONTENT"}]]
+  (gen-structure "ORU_R01")
 
   (-> (io/file "test/hl7v2/data/ACK.xsd")
       (parse-xsd)
@@ -158,62 +168,14 @@
 
   (-> (io/file "tmp/HL7-xml v2.5.1/segments.xsd")
       (parse-xsd)
-      (index-schema)
-      (get "PID.CONTENT")
-      #_(sequence-item))
+      (index-schema))
 
   (-> (io/file "tmp/HL7-xml v2.5.1/datatypes.xsd")
       (parse-xsd)
-      (index-schema)
-      (get "XPN")
-      #_(sequence-item))
-
-  (-> (io/file "tmp/HL7-xml v2.5.1/datatypes.xsd")
-      (parse-xsd)
-      (index-schema)
-      (get "FN")
-      #_(sequence-item))
+      (index-schema))
 
   (-> (io/file "tmp/HL7-xml v2.5.1/fields.xsd")
       (parse-xsd)
-      (index-schema)
-      (get "PID.5.CONTENT")
-      #_(content-item))
-  ;;=> ["PID.5.CONTENT"
-  ;;    ["annotation"
-  ;;     ["documentation" {:lang "en"} "Patient Name"]
-  ;;     ["documentation" {:lang "de"} "Patientenname"]
-  ;;     ["appinfo" ["Item" "108"] ["Type" "XPN"] ["LongName" "HL7Patient\n                Name"]]]
-  ;;    ["complexContent" ["extension" {:base "XPN"} ["PID.5.ATTRIBUTES"]]]]
-
-  (-> (io/file "tmp/HL7-xml v2.5.1/datatypes.xsd")
-      (parse-xsd)
-      (index-schema)
-      (get "XPN.1.CONTENT")
-      #_(content-item))
-  ;;=> ["XPN.1.CONTENT"
-  ;;    ["annotation"
-  ;;     ["documentation" {:lang "en"} "Family Name"]
-  ;;     ["documentation" {:lang "de"} "Familienname"]
-  ;;     ["appinfo" ["Type" "XPN.1"] ["LongName" "Family Name"]]]
-  ;;    ["complexContent" ["extension" {:base "FN"} ["XPN.1.ATTRIBUTES"]]]]
-
-  (-> (io/file "tmp/HL7-xml v2.5.1/datatypes.xsd")
-      (parse-xsd)
-      (index-schema)
-      (get "FN.1.CONTENT")
-      #_(content-item))
-  ;;=> ["FN.1.CONTENT"
-  ;;    ["annotation"
-  ;;     ["documentation" {:lang "en"} "Surname"]
-  ;;     ["documentation" {:lang "de"} "gesamter Nachname"]
-  ;;     ["appinfo" ["Type" "FN.1"] ["LongName" "Surname"]]]
-  ;;    ["complexContent" ["extension" {:base "ST"} ["FN.1.ATTRIBUTES"]]]]
-
-  (-> (io/file "tmp/HL7-xml v2.5.1/datatypes.xsd")
-      (parse-xsd)
-      (index-schema)
-      (get "ST"))
-  ;;=> ["ST" ["restriction" {:base "xsd:string"}]]
+      (index-schema))
 
   :.)
