c91e50890c68d4197eec805ad0cfa594f6c7efb8
Guillermo <guillerglez88@gmail.com>
2025-01-08 17:41:44 +0100

    regenerating structures, fixing field names

--- dev/user.clj
+++ dev/user.clj
@@ -5,15 +5,15 @@
 
 (comment
 
-  (let [dir "structures/v2.3.1"]
+  (let [dir "structures/v2.5.1"]
     (doseq [item (read-string (slurp (str dir "/index.edn")))]
       (println "generating structure for" item)
       (spit
        (format "%s/%s.edn" dir item)
        (with-out-str
          (pp/pprint (gen-structure item
-                                   :standard-dir "/Users/guille/Downloads/v2.3.1"
-                                   :version "2.3.1"
+                                   :standard-dir "/Users/guille/Downloads/v2.5.1"
+                                   :version "2.5.1"
                                    :lang "en"))))))
 
   :.)
